84c7c3a788331ead57663e04aab5bd03b50eadf1
Pamela Matun <81801851+pmatun@users.noreply.github.com>
2022-04-26 17:36:26 +0200

    Update prepare-env.sh

--- new-scenario-template/prepare-env.sh
+++ new-scenario-template/prepare-env.sh
@@ -2,4 +2,4 @@
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
 docker network create mybridge
-docker run -d --network mybridge -p 5050:5050 -e PGADMIN_SETUP_EMAIL=admin -e PGADMIN_SETUP_PASSWORD=password --name=pgadmin crunchydata/crunchy-pgadmin4:centos7-12.2-4.3.0
+docker run -d --network mybridge -p 5050:5050 -e PGADMIN_SETUP_EMAIL=admin -e PGADMIN_SETUP_PASSWORD=password --name=pgadmin dpage/pgadmin4
