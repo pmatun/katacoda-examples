e61ad183479cc3a37ac746b922f336a1ee615e04
Pamela Matun <81801851+pmatun@users.noreply.github.com>
2022-05-07 21:06:24 +0200

    Update prepare-env.sh

--- new-scenario-template/prepare-env.sh
+++ new-scenario-template/prepare-env.sh
@@ -12,7 +12,7 @@ if [[ ! -d "$PGADMIN_SETUP_EMAIL" ]]; then
 fi
 
 sudo apt update
-sudo apt-get install -y ca-certificates
+
 
 #
 # Setup the repository
@@ -22,7 +22,7 @@ sudo apt-get install -y ca-certificates
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 
 # Create the repository configuration file:
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt-get install -y ca-certificates & sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 
 #
 # Install pgAdmin
