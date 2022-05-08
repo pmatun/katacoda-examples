8d3596d2a869a026e391017346317b4cc6ebfdc6
Pamela Matun <81801851+pmatun@users.noreply.github.com>
2022-05-08 18:01:09 +0200

    Update prepare-env.sh

--- new-scenario-template/prepare-env.sh
+++ new-scenario-template/prepare-env.sh
@@ -36,5 +36,6 @@ sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_
 sudo apt install pgadmin4-web --yes </dev/null
 
 # Configure the webserver, if you installed pgadmin4-web:
-PGADMIN_SETUP_PASSWORD="admin@domain.com" PGADMIN_SETUP_EMAIL="password" /usr/pgadmin4/bin/setup-web.sh --yes
+/usr/pgadmin4/bin/setup-web.sh --yes
+#PGADMIN_SETUP_PASSWORD="admin@domain.com" PGADMIN_SETUP_EMAIL="password" /usr/pgadmin4/bin/setup-web.sh --yes
 #. /usr/pgadmin4/bin/setup-web.sh --yes
