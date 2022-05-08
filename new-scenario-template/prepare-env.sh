4dd2d63f3cf125d8912f47c19583f549dee510fb
Pamela Matun <81801851+pmatun@users.noreply.github.com>
2022-05-08 17:12:35 +0200

    Update prepare-env.sh

--- new-scenario-template/prepare-env.sh
+++ new-scenario-template/prepare-env.sh
@@ -18,10 +18,10 @@ sudo apt-get install ca-certificates -y </dev/null
 #
 
 # Install the public key for the repository (if not done previously):
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+#sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 
 # Create the repository configuration file:
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+#sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 
 #
 # Install pgAdmin
@@ -34,10 +34,10 @@ sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_
 #sudo apt install pgadmin4-desktop
 
 # Install for web mode only: 
-sudo apt install pgadmin4-web --yes </dev/null
+#sudo apt install pgadmin4-web --yes </dev/null
 
 # Configure the webserver, if you installed pgadmin4-web:
 #PGADMIN_SETUP_PASSWORD="admin@domain.com" PGADMIN_SETUP_EMAIL="password" /usr/pgadmin4/bin/setup-web.sh --yes
-. /usr/pgadmin4/bin/setup-web.sh --yes
+#. /usr/pgadmin4/bin/setup-web.sh --yes
 
 #launch.sh
