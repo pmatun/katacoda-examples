ec7938045fdaf088c3cc1aea138e85b22ac07104
Pamela Matun <81801851+pmatun@users.noreply.github.com>
2022-05-11 01:42:13 +0200

    Update prepare-env.sh

--- new-scenario-template/prepare-env.sh
+++ new-scenario-template/prepare-env.sh
@@ -40,8 +40,8 @@ sudo apt install pgadmin4-web --yes </dev/null
 #PGADMIN_SETUP_PASSWORD="admin@domain.com" PGADMIN_SETUP_EMAIL="password" /usr/pgadmin4/bin/setup-web.sh --yes
 #. /usr/pgadmin4/bin/setup-web.sh --yes
 
-sed -i 's/Listen 80/Listen 8081/g' /etc/apache2/ports.conf
+#sed -i 's/Listen 80/Listen 8081/g' /etc/apache2/ports.conf
 
-sed -i 's/*:80/*:8081/g' /etc/apache2/sites-available/000-default.conf
+#sed -i 's/*:80/*:8081/g' /etc/apache2/sites-available/000-default.conf
 
-sudo systemctl restart apache2
+#sudo systemctl restart apache2
